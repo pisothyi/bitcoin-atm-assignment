02c24fe285e079402656e48172521e9ae6803486
Pisoth Yi <pisothyi@gmail.com>
2023-11-22 23:35:45 +0700

    First commit

---
+++ Transaction.java
@@ -0,0 +1,36 @@
+public class Transaction {
+    private String transactionID;
+    private String customerID;
+    private String type;
+    private double totalAmount;
+
+    // Full Constructor
+    public Transaction(String transactionID, String customerID, String type, double totalAmount) {
+        this.transactionID = transactionID;
+        this.customerID = customerID;
+        this.type = type;
+        this.totalAmount = totalAmount;
+    }
+
+    // Accessor methods
+    public String getTransactionID() {
+        return transactionID;
+    }
+
+    public String getCustomerID() {
+        return customerID;
+    }
+
+    public String getTransactionType() {
+        return type;
+    }
+
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+
+    // Mutator methods
+    public void setTotalAmount(double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+}
